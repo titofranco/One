567bf40ebc395abe3a4f98861409975d53748bdf
“Carlos <titofranco@gmail.com>
2010-02-26 15:06:40 -0500

    Se crea el controlador, la vista y un prototipo muy simple de la interfaz, sólo se muestra el mapa.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+
+var map;
+var centerLatitude = 6.2645966700;
+var centerLongitude = -75.5877166080;
+var startZoom = 17;
+
+function init(){
+    if(GBrowserIsCompatible()){
+        map = new GMap2(document.getElementById("map"));
+        map.addControl(new GSmallMapControl());
+        map.setCenter(new GLatLng(centerLatitude,centerLongitude),startZoom);
+    }
+}
+
+window.onload = init;
+window.unonload = GUnload;
+
