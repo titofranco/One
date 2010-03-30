e110b72262e463ee5ecafc87f492aa5d0b909867
“Carlos <titofranco@gmail.com>
2010-03-29 19:09:45 -0500

    Se crea el menu que se despliega cuando el usuario hace clic derecho sobre el mapa

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,19 +4,47 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 // 6.2645966700,-75.5877166080;
-var map;
+$(document).ready(function() {
 var centerLatitude = 6.2645966700;
 var centerLongitude = -75.5877166080;
 var startZoom = 17;
 
-function init(){
-    if(GBrowserIsCompatible()){
-        map = new GMap2(document.getElementById("map"));
+      var map = new google.maps.Map2(document.getElementById("map"));
+
+        map.addControl(new GMapTypeControl());
         map.addControl(new GSmallMapControl());
-        map.setCenter(new GLatLng(centerLatitude,centerLongitude),startZoom);
-    }
+        var location = new GLatLng(centerLatitude,centerLongitude);
+        map.setCenter(location,startZoom);
+
+    $('#map').contextMenu('myMenu', {
+
+      bindings: {
+        'Punto_Inicial': function(t) {
+          alert('Trigger was '+t.id+'\nAction was Punto Inicial');
+        },
+
+        'Punto_Final': function(t) {
+          alert('Trigger was '+t.id+'\nAction was Punto Final');
+        },
+      }
+    }); //context menu end
+});
+
+function addMarker(latitude,longitude,description){
+
+    var marker = new Gmarker(new GLatLng(centerLatitude,centerLongitude))
+
+    GEvent.addListener(marker,'click',
+    function(){
+    marker.openInfoWindowHtml(description)
+    })
+
+    map.addOverlay(marker);
+
 }
 
-window.onload = init;
+//Pagina 9 capitulo 3 para retornar la latitud longitud del marker
+//overlay y latlng son variables ya definidas por google
+//allow the user to click the map to create a marker
 window.unonload = GUnload;
 
