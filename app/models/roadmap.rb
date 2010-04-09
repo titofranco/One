1c27caf56098237e9f924665e2d3ed01d3909181
“Carlos <titofranco@gmail.com>
2010-04-08 23:19:21 -0500

    metodos de validacion-fallidos

--- app/models/roadmap.rb
+++ app/models/roadmap.rb
@@ -1,2 +1,11 @@
 class Roadmap < ActiveRecord::Base
+
+  validates_presence_of :initial_point, :end_point
+  validate :latitud_longitud
+
+  def latitud_longitud(initial_point)
+    puts"fdsfdsfdsfdsfdsfdsfdsfds #{initial_point}"
+    errors.add(:initial_point,'No debe ser nulo') if initial_point.nil?
+  end
 end
+
