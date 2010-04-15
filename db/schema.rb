a3129ddd7720b495009e6048fce2214939c0a601
“Carlos <titofranco@gmail.com>
2010-04-15 17:39:47 -0500

    Se modifica el schema para quitar timestamp de la tabla roadmaps

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(:version => 20100316040019) do
     t.string   "bus_stop_downtown"
     t.decimal  "route_length_km",   :precision => 5, :scale => 5
     t.string   "routes_taken"
-    t.datetime "created_at",                                      :default => '2010-03-15 22:56:58'
-    t.datetime "updated_at",                                      :default => '2010-03-15 22:56:58'
+    t.datetime "created_at",                                      :default => '2010-04-13 19:50:49'
+    t.datetime "updated_at",                                      :default => '2010-04-13 19:50:49'
   end
 
   create_table "buses_routes", :force => true do |t|
@@ -28,23 +28,21 @@ ActiveRecord::Schema.define(:version => 20100316040019) do
   end
 
   create_table "roadmaps", :force => true do |t|
-    t.string   "way_type",     :limit => 13
-    t.string   "street_name",  :limit => 50
-    t.string   "common_name",  :limit => 50
-    t.string   "municipality", :limit => 20
-    t.string   "prefix",       :limit => 2
-    t.string   "label",        :limit => 20
-    t.decimal  "shape_length",               :precision => 15, :scale => 10
-    t.decimal  "lat_start",                  :precision => 15, :scale => 10, :null => false
-    t.decimal  "lat_center",                 :precision => 15, :scale => 10
-    t.decimal  "lat_end",                    :precision => 15, :scale => 10, :null => false
-    t.decimal  "long_start",                 :precision => 15, :scale => 10, :null => false
-    t.decimal  "long_center",                :precision => 15, :scale => 10
-    t.decimal  "long_end",                   :precision => 15, :scale => 10, :null => false
-    t.integer  "stretch_type",                                               :null => false
-    t.decimal  "speed_km_h",                 :precision => 15, :scale => 10
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string  "way_type",     :limit => 13
+    t.string  "street_name",  :limit => 50
+    t.string  "common_name",  :limit => 50
+    t.string  "municipality", :limit => 20
+    t.string  "prefix",       :limit => 2
+    t.string  "label",        :limit => 20
+    t.decimal "shape_length",               :precision => 15, :scale => 10
+    t.decimal "lat_start",                  :precision => 15, :scale => 10, :null => false
+    t.decimal "lat_center",                 :precision => 15, :scale => 10
+    t.decimal "lat_end",                    :precision => 15, :scale => 10, :null => false
+    t.decimal "long_start",                 :precision => 15, :scale => 10, :null => false
+    t.decimal "long_center",                :precision => 15, :scale => 10
+    t.decimal "long_end",                   :precision => 15, :scale => 10, :null => false
+    t.integer "stretch_type",                                               :null => false
+    t.decimal "speed_km_h",                 :precision => 15, :scale => 10
   end
 
   add_index "roadmaps", ["lat_end"], :name => "index_roadmaps_on_lat_end"
