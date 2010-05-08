edb190ee1a84bf289b59be8128bf026649255d88
“Carlos <titofranco@gmail.com>
2010-05-08 17:31:11 -0500

    Las llamadas a la BD se pasaron para el modelo

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -14,113 +14,36 @@ def initialize
 #@a= ["1","3","2","6","14","17","10","11","16","15","27","23","24","21","18","26","25","34"] #esta malo
   #@a = ["1","3","6","14","18","21","24","27","23","28","25","34","36","48"]
   #@a = ["1","3","6","14","18","21","24","22","17","10","11","16","20","15","27","23","28","25","34","36","48"]
+@init_point
+@end_point
 end
 
 def calcular
 
-  param_initial_point = params[:initial_point]
-  param_end_point = params[:end_point]
-  lat_start,long_start = param_initial_point.split(/,/)
-  lat_end,long_end = param_end_point.split(/,/)
-  eval_points = get_closest_points(lat_start,long_start,lat_end,long_end)
-  if(eval_points.to_s.eql?("true"))
-     resultadoquery = metodoruta(lat_start,long_start,lat_end,long_end)
-     res={:success=>true, :content=>resultadoquery}
-     render :text=>res.to_json
-  else
-    res={:success=>false, :content=>eval_points}
+  params_initial_point = params[:initial_point]
+  params_end_point = params[:end_point]
+  lat_start,long_start = params_initial_point.split(/,/)
+  lat_end,long_end = params_end_point.split(/,/)
+  closest_init_point = Roadmap.get_closest_init_point(lat_start,long_start)
+  if(closest_init_point.empty?)
+    res={:success=>false, :content=>"Debe de elegir un punto inicial más cercano"}
     render :text=>res.to_json
-  end
-end
-
-def get_closest_points(lat_start,long_start,lat_end,long_end)
-  #puts "el lat-long start #{lat_start},#{long_start} el lat-long_end #{lat_end},#{long_end} "
-  #Ambos queries tienen un radio equivalente a 500 metros equivalente a 0.310685596 millas
-  sql = "select id, dest.lat_start,dest.long_start,3956 * 2 * ASIN(SQRT(POWER(SIN((" +lat_start+
-        "-abs(dest.lat_start)) * pi()/180 / 2), 2) +  COS("+lat_start+" * pi()/180 ) *
-        COS(abs(dest.lat_start) * pi()/180) * POWER(SIN(("+long_start+ "- dest.long_start) *
-        pi()/180 / 2), 2) )) as  distance
-        FROM roadmaps dest
-        having distance < 0.310685596
-        order by distance limit 1"
-  @init_point = Roadmap.find_by_sql(sql)
-
-  if(@init_point.empty?)
-    return "Debe elegir un punto inicial más ceracano"
   else
-    sql = "select id,3956 * 2 * ASIN(SQRT(POWER(SIN((" +lat_end+ "-abs(dest.lat_start)) *
-          pi()/180 / 2), 2) +  COS("+lat_end+" * pi()/180 ) *
-          COS(abs(dest.lat_start) * pi()/180) * POWER(SIN(("+long_end+ "- dest.long_start) *
-          pi()/180 / 2), 2) )) as  distance
-          FROM roadmaps dest
-          where dest.lat_start not in("+@init_point[0].lat_start.to_s+") and dest.long_start not in ("+@init_point[0].long_start.to_s+")
-          having distance < 0.310685596
-          order by distance limit 1"
-    @end_point = Roadmap.find_by_sql(sql)
-    if(@end_point.empty?)
-      return "Debe elegir un punto final más cercano"
+    closest_end_point = Roadmap.get_closest_end_point(lat_end,long_end)
+    if(closest_end_point.empty?)
+      res={:success=>false, :content=>"Debe de elegir un punto final más cercano"}
+      render :text=>res.to_json
+    else
+      resultadoquery = metodoruta(lat_start,long_start,lat_end,long_end)
+      res={:success=>true, :content=>resultadoquery}
+      render :text=>res.to_json
     end
   end
-
-return "true"
-
 end
+
 
 def metodoruta(lat_start,long_start,lat_end,long_end)
-
-  resultado = Array.new
-
-  # Se debe hacer conexion con el punto inicial escogido por el usuario y el primer nodo en el grafo
-  point_a = Roadmap.find(@init_point[0].id,
-                        :select=> "way_type,street_name,prefix,label,common_name,lat_start,long_start")
-  resultado.push({:id=>00000,:lat_start=>lat_start, :long_start=>long_start,
-  :lat_end=>point_a.lat_start,:long_end=>point_a.long_start,:stretch_type=>1,
-  :way_type_a=>" ",:street_name_a=>" ",:prefix_a=>" ",:label_a=>" ",
-  :common_name_a=>" ",:way_type_b=>point_a.way_type,:street_name_b=>point_a.street_name,
-  :prefix_b=>point_a.prefix,:label_b=>point_a.label,:common_name_b=>point_a.common_name})
-  #puts "El resultado del query para el punto mas cercano inicial #{init_point}"
-#QuerY para pintar lineas del metro, cada una se debe pintar a parte mejor
-=begin
-  r = Roadmap.find(:all,:conditions => ['stretch_type in (?) AND municipality in (?)',[3],['MEDELLIN']])
-    r.each do |s|
-      resultado.push({:id=>s.id,
-      :lat_start=>s.lat_start, :long_start=>s.long_start, :lat_end=>s.lat_end, :long_end=>s.long_end,
-      :stretch_type=>s.stretch_type })
-    end
-=end
-  for i in 0 ...@a.length-1
-    if StreetRelation.exists?(:roadmap_id=>@a[i], :roadmap_related_id=>@a[i+1])
-    route = StreetRelation.find_by_sql ["Select s.id,s.lat_start,s.long_start,s.lat_end,s.long_end,s.stretch_type, "+
-                                       "a.way_type as way_type_a,a.street_name as street_name_a, "+
-                                       "a.prefix as prefix_a,a.label as label_a,a.common_name as common_name_a, "+
-                                       "b.way_type as way_type_b,b.street_name as street_name_b, "+
-                                       "b.prefix as prefix_b,b.label as label_b, b.common_name as common_name_b "+
-                                       "from street_relations s "+
-                                       "inner join roadmaps as a "+
-                                       "on(a.id = s.roadmap_id) "+
-                                       "inner join roadmaps as b "+
-                                       "on(b.id = s.roadmap_related_id) "+
-                                       "where roadmap_id = ? AND roadmap_related_id = ?",@a[i],@a[i+1]]
-
-    route = route[0]
-    resultado.push({:id=>route.id,:lat_start=>route.lat_start, :long_start=>route.long_start,
-    :lat_end=>route.lat_end,:long_end=>route.long_end,:stretch_type=>route.stretch_type,
-    :way_type_a=>route.way_type_a,:street_name_a=>route.street_name_a,:prefix_a=>route.prefix_a,
-    :label_a=>route.label_a,:common_name_a=>route.common_name_a,:way_type_b=>route.way_type_b,:street_name_b=>route.street_name_b,
-    :prefix_b=>route.prefix_b,:label_b=>route.label_b,:common_name_b=>route.common_name_b})
-    else
-      puts "El registro para roadmap_id #{@a[i]} roadmap_related_id:#{@a[i+1]} no está en la BD"
-    end
-  end
- # puts "El ultimo registro es #{route.id} #{route.street_name_b}"
-
-  #El ultimo registro en el array es el que va conectado con el punto final escogido por el usuario
-  #last_point = Roadmap.find(@end_point[0].id,:select=> "way_type,street_name,prefix,label,lat_start,long_start")
-  resultado.push({:id=>99999,:lat_start=>route.lat_end, :long_start=>route.long_end,
-  :lat_end=>lat_end,:long_end=>long_end,:stretch_type=>1,:way_type_a=>route.way_type_b,
-  :street_name_a=>route.street_name_b,:prefix_a=>route.prefix_b,:label_a=>route.label_a,
-  :common_name_a=>route.common_name_a,:way_type_b=>" ",:street_name_b=>" ",:prefix_b=>" ",
-  :label_b=>" ",:common_name_b=>" "})
+  resultado = Roadmap.getRoute(@a,lat_start,long_start,lat_end,long_end)
   resultado
 end
 
--- app/models/roadmap.rb
+++ app/models/roadmap.rb
@@ -1,11 +1,94 @@
 class Roadmap < ActiveRecord::Base
 
-  validates_presence_of :initial_point, :end_point
-  validate :latitud_longitud
-
-  def latitud_longitud(initial_point)
-    puts"fdsfdsfdsfdsfdsfdsfdsfds #{initial_point}"
-    errors.add(:initial_point,'No debe ser nulo') if initial_point.nil?
+  def initialize
+  @init_point
+  @end_point
   end
+
+  #Ambos queries tienen un radio equivalente a 500 metros equivalente a 0.310685596 millas
+  def self.get_closest_init_point(lat_start,long_start)
+    sql = "select id, dest.lat_start,dest.long_start,3956 * 2 * ASIN(SQRT(POWER(SIN((" +lat_start+
+          "-abs(dest.lat_start)) * pi()/180 / 2), 2) +  COS("+lat_start+" * pi()/180 ) *
+          COS(abs(dest.lat_start) * pi()/180) * POWER(SIN(("+long_start+ "- dest.long_start) *
+          pi()/180 / 2), 2) )) as  distance
+          FROM roadmaps dest
+          having distance < 0.310685596
+          order by distance limit 1"
+    @init_point = find_by_sql(sql)
+    @init_point
+  end
+
+  #Ambos queries tienen un radio equivalente a 500 metros equivalente a 0.310685596 millas
+  def self.get_closest_end_point(lat_end,long_end)
+    sql = "select id,3956 * 2 * ASIN(SQRT(POWER(SIN((" +lat_end+ "-abs(dest.lat_start)) *
+          pi()/180 / 2), 2) +  COS("+lat_end+" * pi()/180 ) *
+          COS(abs(dest.lat_start) * pi()/180) * POWER(SIN(("+long_end+ "- dest.long_start) *
+          pi()/180 / 2), 2) )) as  distance
+          FROM roadmaps dest
+          where dest.lat_start not in("+@init_point[0].lat_start.to_s+") and dest.long_start not in ("+@init_point[0].long_start.to_s+")
+          having distance < 0.310685596
+          order by distance limit 1"
+    @end_point = find_by_sql(sql)
+    @end_point
+  end
+
+  def self.getRoute(nodes,lat_start,long_start,lat_end,long_end)
+    resultado = Array.new
+    # Se debe hacer conexion con el punto inicial escogido por el usuario y el primer nodo en el grafo
+    point_a = find(@init_point[0].id,
+                          :select=> "way_type,street_name,prefix,label,common_name,lat_start,long_start")
+    resultado.push({:id=>00000,:lat_start=>lat_start, :long_start=>long_start,
+    :lat_end=>point_a.lat_start,:long_end=>point_a.long_start,:stretch_type=>1,
+    :way_type_a=>" ",:street_name_a=>" ",:prefix_a=>" ",:label_a=>" ",
+    :common_name_a=>" ",:way_type_b=>point_a.way_type,:street_name_b=>point_a.street_name,
+    :prefix_b=>point_a.prefix,:label_b=>point_a.label,:common_name_b=>point_a.common_name})
+    #puts "El resultado del query para el punto mas cercano inicial #{init_point}"
+    #QuerY para pintar lineas del metro, cada una se debe pintar a parte mejor
+=begin
+      r = Roadmap.find(:all,:conditions => ['stretch_type in (?) AND municipality in (?)',[3],['MEDELLIN']])
+        r.each do |s|
+          resultado.push({:id=>s.id,
+          :lat_start=>s.lat_start, :long_start=>s.long_start, :lat_end=>s.lat_end, :long_end=>s.long_end,
+          :stretch_type=>s.stretch_type })
+        end
+=end
+    for i in 0 ...nodes.length-1
+      if StreetRelation.exists?(:roadmap_id=>nodes[i], :roadmap_related_id=>nodes[i+1])
+      route = StreetRelation.find_by_sql ["Select s.id,s.lat_start,s.long_start,s.lat_end,s.long_end,s.stretch_type, "+
+                                         "a.way_type as way_type_a,a.street_name as street_name_a, "+
+                                         "a.prefix as prefix_a,a.label as label_a,a.common_name as common_name_a, "+
+                                         "b.way_type as way_type_b,b.street_name as street_name_b, "+
+                                         "b.prefix as prefix_b,b.label as label_b, b.common_name as common_name_b "+
+                                         "from street_relations s "+
+                                         "inner join roadmaps as a "+
+                                         "on(a.id = s.roadmap_id) "+
+                                         "inner join roadmaps as b "+
+                                         "on(b.id = s.roadmap_related_id) "+
+                                         "where roadmap_id = ? AND roadmap_related_id = ?",nodes[i],nodes[i+1]]
+
+      route = route[0]
+      resultado.push({:id=>route.id,:lat_start=>route.lat_start, :long_start=>route.long_start,
+      :lat_end=>route.lat_end,:long_end=>route.long_end,:stretch_type=>route.stretch_type,
+      :way_type_a=>route.way_type_a,:street_name_a=>route.street_name_a,:prefix_a=>route.prefix_a,
+      :label_a=>route.label_a,:common_name_a=>route.common_name_a,:way_type_b=>route.way_type_b,:street_name_b=>route.street_name_b,
+      :prefix_b=>route.prefix_b,:label_b=>route.label_b,:common_name_b=>route.common_name_b})
+      else
+        puts "El registro para roadmap_id #{nodes[i]} roadmap_related_id:#{nodes[i+1]} no está en la BD"
+      end
+    end
+   # puts "El ultimo registro es #{route.id} #{route.street_name_b}"
+
+    #El ultimo registro en el array es el que va conectado con el punto final escogido por el usuario
+    #last_point = Roadmap.find(@end_point[0].id,:select=> "way_type,street_name,prefix,label,lat_start,long_start")
+    resultado.push({:id=>99999,:lat_start=>route.lat_end, :long_start=>route.long_end,
+    :lat_end=>lat_end,:long_end=>long_end,:stretch_type=>1,:way_type_a=>route.way_type_b,
+    :street_name_a=>route.street_name_b,:prefix_a=>route.prefix_b,:label_a=>route.label_a,
+    :common_name_a=>route.common_name_a,:way_type_b=>" ",:street_name_b=>" ",:prefix_b=>" ",
+    :label_b=>" ",:common_name_b=>" "})
+
+    resultado
+  end
+
+
 end
 
