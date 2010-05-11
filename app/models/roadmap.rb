3332b7edf0b5b6f2689ff694d741aec88a80825c
“Carlos <titofranco@gmail.com>
2010-05-10 21:56:56 -0500

    Se valida que no se inserten registros donde la distancia sea cero

--- app/models/roadmap.rb
+++ app/models/roadmap.rb
@@ -40,7 +40,7 @@ class Roadmap < ActiveRecord::Base
     resultado.push({:id=>00000,:lat_start=>lat_start, :long_start=>long_start,
     :lat_end=>point_a.lat_start,:long_end=>point_a.long_start,:stretch_type=>1,
     :way_type_a=>" ",:street_name_a=>" ",:prefix_a=>" ",:label_a=>" ",
-    :common_name_a=>" ",:way_type_b=>point_a.way_type,:street_name_b=>point_a.street_name,
+    :common_name_a=>" ",:distance=>1, :way_type_b=>point_a.way_type,:street_name_b=>point_a.street_name,
     :prefix_b=>point_a.prefix,:label_b=>point_a.label,:common_name_b=>point_a.common_name})
     #puts "El resultado del query para el punto mas cercano inicial #{init_point}"
     #QuerY para pintar lineas del metro, cada una se debe pintar a parte mejor
@@ -56,7 +56,7 @@ class Roadmap < ActiveRecord::Base
       if StreetRelation.exists?(:roadmap_id=>nodes[i], :roadmap_related_id=>nodes[i+1])
       route = StreetRelation.find_by_sql ["Select s.id,s.lat_start,s.long_start,s.lat_end,s.long_end,s.stretch_type, "+
                                          "a.way_type as way_type_a,a.street_name as street_name_a, "+
-                                         "a.prefix as prefix_a,a.label as label_a,a.common_name as common_name_a, "+
+                                         "a.prefix as prefix_a,a.label as label_a,a.common_name as common_name_a,s.distance_meters, "+
                                          "b.way_type as way_type_b,b.street_name as street_name_b, "+
                                          "b.prefix as prefix_b,b.label as label_b, b.common_name as common_name_b "+
                                          "from street_relations s "+
@@ -67,11 +67,13 @@ class Roadmap < ActiveRecord::Base
                                          "where roadmap_id = ? AND roadmap_related_id = ?",nodes[i],nodes[i+1]]
 
       route = route[0]
+      if(route.distance_meters >0)
       resultado.push({:id=>route.id,:lat_start=>route.lat_start, :long_start=>route.long_start,
       :lat_end=>route.lat_end,:long_end=>route.long_end,:stretch_type=>route.stretch_type,
       :way_type_a=>route.way_type_a,:street_name_a=>route.street_name_a,:prefix_a=>route.prefix_a,
-      :label_a=>route.label_a,:common_name_a=>route.common_name_a,:way_type_b=>route.way_type_b,:street_name_b=>route.street_name_b,
-      :prefix_b=>route.prefix_b,:label_b=>route.label_b,:common_name_b=>route.common_name_b})
+      :label_a=>route.label_a,:common_name_a=>route.common_name_a,:distance=>route.distance_meters,:way_type_b=>route.way_type_b,
+      :street_name_b=>route.street_name_b,:prefix_b=>route.prefix_b,:label_b=>route.label_b,:common_name_b=>route.common_name_b})
+      end
       else
         puts "El registro para roadmap_id #{nodes[i]} roadmap_related_id:#{nodes[i+1]} no está en la BD"
       end
@@ -83,7 +85,7 @@ class Roadmap < ActiveRecord::Base
     resultado.push({:id=>99999,:lat_start=>route.lat_end, :long_start=>route.long_end,
     :lat_end=>lat_end,:long_end=>long_end,:stretch_type=>1,:way_type_a=>route.way_type_b,
     :street_name_a=>route.street_name_b,:prefix_a=>route.prefix_b,:label_a=>route.label_a,
-    :common_name_a=>route.common_name_a,:way_type_b=>" ",:street_name_b=>" ",:prefix_b=>" ",
+    :common_name_a=>route.common_name_a,:distance=>1,:way_type_b=>" ",:street_name_b=>" ",:prefix_b=>" ",
     :label_b=>" ",:common_name_b=>" "})
 
     resultado
