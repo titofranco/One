03a9cd8ddb93314a9e83d17d5bfe38dffb303a06
Joan Ruiz <morden.curien@gmail.com>
2010-06-01 14:33:12 -0500

    Dijkstra con heaps y control de distancia

--- app/helpers/tester.rb
+++ app/helpers/tester.rb
@@ -1,16 +1,40 @@
 require "Dijkstra"
-require "DijkstraTemp"
 require "AlgoritmoA"
 require "Parser"
 
 puts "parsing..."
 streets = Parser.getGrafo "listas.txt"
 puts "numero de calles: #{streets.size}"
-inicio = Time.now
-puts "inicio: #{inicio}"
-camino = DijkstraTemp.encontrarCamino streets,1,5253
-fin = Time.now
-puts "fin: #{fin}"
-puts "se demoro #{(fin-inicio)/60}"
+#Carlos-Joan
+puts "inicio: #{Time.now}"
+camino = Dijkstra.encontrarCamino streets,20235,6499
+puts "fin: #{Time.now}"
 puts camino.inspect
+puts camino.size
 
+#Joan-Carlos
+# puts "inicio: #{Time.now}"
+# camino = Dijkstra.encontrarCamino streets,6499,20235
+# puts "fin: #{Time.now}"
+# puts camino.inspect
+# puts camino.size
+
+#ruta 4
+# puts "inicio: #{Time.now}"
+# camino = Dijkstra.encontrarCamino streets,30060,13729
+# puts "fin: #{Time.now}"
+# puts camino.inspect
+# puts camino.size
+
+#test
+# puts "inicio: #{Time.now}"
+# camino = Dijkstra.encontrarCamino streets,12668,6499
+# puts "fin: #{Time.now}"
+# puts camino.inspect
+# puts camino.size
+
+f = File.new("camino","w+")
+camino.each{ |l|
+  f.write(l.to_s.concat(","))
+}
+f.close
