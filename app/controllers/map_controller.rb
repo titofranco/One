4a231866a6e4ea9e4ba4024a914535ff39697311
Joan Ruiz <morden.curien@gmail.com>
2010-06-19 16:06:06 -0500

    Cambio de nombres a algunas variables

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,59 +1,47 @@
 class MapController < ApplicationController
-
+  
   def calcular
+    
     params_initial_point = params[:initial_point]
     params_end_point = params[:end_point]
     lat_start,long_start = params_initial_point.split(/,/)
     lat_end,long_end = params_end_point.split(/,/)
-    closest_init_point = Roadmap.get_closest_init_point(lat_start,long_start)
-    if(closest_init_point.empty?)
+
+    @closest_init_point =
+    Roadmap.get_closest_init_point(lat_start,long_start)
+    if(@closest_init_point.empty?)
       res={:success=>false, :content=>"Debe de elegir un punto inicial más cercano"}
       render :text=>res.to_json
     else
-      closest_end_point = Roadmap.get_closest_end_point(lat_end,long_end)
-      if closest_end_point.empty? 
+      @closest_end_point = Roadmap.get_closest_end_point(lat_end,long_end)
+      if @closest_end_point.empty? 
         res={:success=>false, :content=>"Debe de elegir un punto final más cercano"}
         render :text=>res.to_json
       else        
         #dijkstra
         puts "parsing..."
         streets = Parser.getGrafo "#{RAILS_ROOT}/lib/dijkstra/listas.txt"
-        puts "numero de calles: #{streets.size}"
-        #Carlos-Joan
-        puts "camino de #{closest_init_point.first.id} a #{closest_end_point.first.id}"
-        puts "inicio: #{Time.now}"
-        @a = Dijkstra.encontrarCamino streets,closest_init_point.first.id,closest_end_point.first.id
-        puts "fin: #{Time.now}"
+        @pathDijkstra = Dijkstra.encontrarCamino streets,@closest_init_point.first.id,@closest_end_point.first.id
         #end dijkstra
-        if @a.size == 0
-          puts "ACA ES FALSO"
+
+        if @pathDijkstra.size == 0
           res={:success=>false, :content=>"Ruta no encontrada"}
           render :text=>res.to_json
         else
-          puts "ACA ES VERDADERO"
           resultadoquery = getRuta(lat_start,long_start,lat_end,long_end)
           res={:success=>true, :content=>resultadoquery}
           render :text=>res.to_json
-          #encontrar rutas de buses
-          
-          @ruta = encontrarBusUnico closest_init_point,closest_end_point
-          puts @ruta==nil
-          if !@ruta
-            puts "gg, le toca coger varios buses"
-          else
-            
-          end
         end
       end
     end
   end
-
-  def omgBus
+  
+  def parserRouteBus busRoute
     puts "oh hai, estoy en omgRuta"
     resultado = Array.new
-    puts @ruta == nil
-    puts "tamano de @ruta: #{@ruta.size}"
-    for bus in @ruta
+    puts busRoute == nil
+    puts "tamano de @ruta: #{busRoute.size}"
+    for bus in busRoute
       #puts "bus: #{bus.inspect}"
       resultado.push(:id=>bus.id,
                      :bus_id=>bus.bus_id,
@@ -65,20 +53,16 @@ class MapController < ApplicationController
     resultado
   end
   
-  def encontrarBusUnico puntoInicial,puntoFinal
+  def findUniqueBus 
     nodoInicial = Roadmap.find(:all,:select=>"lat_start,long_start",
                                :conditions=>["lat_start = ? and long_start = ?",
-                                             puntoInicial.first.lat_start,
-                                             puntoInicial.first.long_start])
+                                             @closest_init_point.first.lat_start,
+                                             @closest_init_point.first.long_start])
     
     nodoFinal = Roadmap.find(:all,:select=>"lat_start,long_start",
                              :conditions=>["lat_start = ? and long_start = ?",
-                                           puntoFinal.first.lat_start,
-                                           puntoFinal.first.long_start])
-    
-    # puts "nodos iniciales #{nodoInicial.inspect}"
-    # puts "nodos finales #{nodoFinal.inspect}"
-    
+                                           @closest_end_point.first.lat_start,
+                                           @closest_end_point.first.long_start])
     rutasInicial = Array.new
     rutasFinal = Array.new
     # puts "Inicio"
@@ -107,15 +91,13 @@ class MapController < ApplicationController
   end
   
   def getRuta(lat_start,long_start,lat_end,long_end)
-    resultado = Roadmap.getRoute(@a,lat_start,long_start,lat_end,long_end)
+    resultado = Roadmap.getRoute(@pathDijkstra,lat_start,long_start,lat_end,long_end)
     resultado
   end
-  
-  def calcularRuta
-  end
-  
+
   def findRouteBuses
-    resultado_bus = omgBus
+    busRoute = findUniqueBus
+    resultado_bus = parserRouteBus busRoute
     if resultado_bus.empty?
       res={:success=>false,:content=>"No se encontró ninguna ruta de bus"}
     else
