0f73156f9528e64ac24d053a19702534fee88459
“Carlos <titofranco@gmail.com>
2010-07-12 19:25:09 -0500

    Se agrega metodo que busca si existe una conexion directa entre un cjto de bus_id

--- app/models/buses_route.rb
+++ app/models/buses_route.rb
@@ -13,5 +13,17 @@ class BusesRoute < ActiveRecord::Base
     end
     resultado
  end
+
+def self.get_common_bus(bus_id_A, bus_id_B)
+
+  sql = "Select distinct br.bus_id as bus_id_A, br2.bus_id as bus_id_B
+        from buses_routes br
+        inner join buses_routes br2
+        On (br.roadmap_id = br2.roadmap_id)
+        where br.bus_id in (" + bus_id_A.to_s + ") and br2.bus_id in (" + bus_id_B + ")" +
+        " having bus_id_A <> bus_id_B "
+  r = find_by_sql(sql)
+end
+
 end
 
