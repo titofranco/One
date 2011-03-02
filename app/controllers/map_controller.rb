902e5e5b643e97c903c053376484ffa06d6015f0
Joan Ruiz <morden.curien@gmail.com>
2011-03-02 10:41:58 -0500

    fixed bug of no bus suggest

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -24,17 +24,16 @@ class MapController < ApplicationController
       #new controller
       bus_route = BusesRoute.get_bus_route(closest_init,closest_final)
       info_bus = BusesRoute.parser_route_bus bus_route
-      bus_explain = SidePannel.explainBusRoute(info_bus)  
-      
+      if !info_bus.empty?
+        bus_explain = SidePannel.explainBusRoute(info_bus)  
+      end     
      
       res={:success=>true,
         :content=>path[:info_path],
         :bus=>info_bus,
         :route_explain => route_explain,
         :bus_explain => bus_explain
-      }
-      
-      
+      }    
 
       render :text=>res.to_json
       
